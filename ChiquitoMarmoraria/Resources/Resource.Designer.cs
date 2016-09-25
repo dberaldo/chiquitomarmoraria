4c95c035127516b411861eee600829864e9de339
Daniela Beraldo <danielaberaldo012@gmail.com>
2016-09-25 17:34:11 -0300

    SQLite + Classe Pessoa

--- ChiquitoMarmoraria/Resources/Resource.Designer.cs
+++ ChiquitoMarmoraria/Resources/Resource.Designer.cs
@@ -63,29 +63,56 @@ namespace ChiquitoMarmoraria
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int button1 = 2131034117;
+			// aapt resource value: 0x7f050009
+			public const int button1 = 2131034121;
 			
-			// aapt resource value: 0x7f050000
-			public const int img_logo = 2131034112;
-			
-			// aapt resource value: 0x7f050007
-			public const int lbl_cadastre = 2131034119;
+			// aapt resource value: 0x7f05000a
+			public const int button2 = 2131034122;
 			
 			// aapt resource value: 0x7f050006
-			public const int lbl_esqueci = 2131034118;
+			public const int editText1 = 2131034118;
 			
-			// aapt resource value: 0x7f050001
-			public const int lbl_login = 2131034113;
+			// aapt resource value: 0x7f050008
+			public const int editText2 = 2131034120;
+			
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f05000b
+			public const int img_logo = 2131034123;
+			
+			// aapt resource value: 0x7f050010
+			public const int lbl_cadastre = 2131034128;
 			
 			// aapt resource value: 0x7f050003
-			public const int lbl_senha = 2131034115;
+			public const int lbl_email = 2131034115;
 			
-			// aapt resource value: 0x7f050002
-			public const int txt_login = 2131034114;
+			// aapt resource value: 0x7f05000f
+			public const int lbl_esqueci = 2131034127;
+			
+			// aapt resource value: 0x7f05000c
+			public const int lbl_login = 2131034124;
+			
+			// aapt resource value: 0x7f050001
+			public const int lbl_nome = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int lbl_senha = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int lbl_senha_repete = 2131034119;
 			
 			// aapt resource value: 0x7f050004
-			public const int txt_senha = 2131034116;
+			public const int txt_email = 2131034116;
+			
+			// aapt resource value: 0x7f05000d
+			public const int txt_login = 2131034125;
+			
+			// aapt resource value: 0x7f050002
+			public const int txt_name = 2131034114;
+			
+			// aapt resource value: 0x7f05000e
+			public const int txt_senha = 2131034126;
 			
 			static Id()
 			{
@@ -101,7 +128,10 @@ namespace ChiquitoMarmoraria
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int cadastro_login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
