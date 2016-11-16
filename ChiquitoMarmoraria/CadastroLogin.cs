9bc505f97d83aa9b6d90b0764616fc9b29012bdb
Daniela Beraldo <danielaberaldo012@gmail.com>
2016-11-16 11:11:51 -0200

    Alteração cadastro - 2

--- ChiquitoMarmoraria/CadastroLogin.cs
+++ ChiquitoMarmoraria/CadastroLogin.cs
@@ -6,18 +6,36 @@ using Android.Widget;
 using MySql.Data.MySqlClient;
 using System.Data;
 using Android.Content;
+using ChiquitoMarmoraria.Resources.model;
+using Android.Views;
 
 namespace ChiquitoMarmoraria
 {
 	[Activity(Label = "Cadastro")]
 	public class CadastroLogin : Activity
 	{
+        Pessoa p;
+
+        //Cadastro Básico
 		EditText txtNome;
 		EditText txtEmail;
 		EditText txtSenha;
 		EditText txtSenhaRepete;
-		Button btnConfirmar;
+        Button btnProximo;
+
+        //Cadastro Endereço
+        EditText txtCep;
+        EditText txtEstado;
+        EditText txtCidade;
+        EditText txtEndereco;
+        EditText txtNumero;
+        EditText txtComplemento;
+        EditText txtTelefone;
+        Button btnConfirmar;
         Button btnVoltar;
+
+        LinearLayout llBasico;
+        LinearLayout llEndereco;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -25,41 +43,72 @@ namespace ChiquitoMarmoraria
             
 			SetContentView(Resource.Layout.CadastroLogin);
 
+            p = new ChiquitoMarmoraria.Resources.model.Pessoa();
+
+            llBasico = FindViewById<LinearLayout>(Resource.Id.ll_infoBasic);
+            llEndereco = FindViewById<LinearLayout>(Resource.Id.ll_infoContato);
+
+            //Cadastro Básico
 			txtNome = FindViewById<EditText>(Resource.Id.txt_nome);
 			txtEmail = FindViewById<EditText>(Resource.Id.txt_email);
 			txtSenha = FindViewById<EditText>(Resource.Id.txt_senha);
 			txtSenhaRepete = FindViewById<EditText>(Resource.Id.txt_senha_repete);
+            btnProximo = FindViewById<Button>(Resource.Id.btn_proximo);
+
+            //Cadastro Endereço
+            txtCep = FindViewById<EditText>(Resource.Id.txt_cep);
+            txtEstado = FindViewById<EditText>(Resource.Id.txt_estado);
+            txtCidade = FindViewById<EditText>(Resource.Id.txt_cidade);
+            txtEndereco = FindViewById<EditText>(Resource.Id.txt_endereco);
+            txtNumero = FindViewById<EditText>(Resource.Id.txt_numero);
+            txtComplemento = FindViewById<EditText>(Resource.Id.txt_complemento);
 			btnConfirmar = FindViewById<Button>(Resource.Id.btn_confirmar);
             btnVoltar = FindViewById<Button>(Resource.Id.btn_voltar);
 
-			btnConfirmar.Click += (object sender, EventArgs e) =>
-			{
+            btnProximo.Click += (object sender, EventArgs e) =>
+            {
+                p.Nome = txtNome.Text;
+                p.Email = txtEmail.Text;
+                p.Senha = txtSenha.Text;
                 if (txtSenha.Text != txtSenhaRepete.Text)
                 {
                     txtSenha.Text = "";
                     txtSenhaRepete.Text = "";
                     Toast.MakeText(this, "Senhas não conferem. Por favor, tente novamente!", ToastLength.Short).Show();
-                    
                 }
                 else
                 {
-                    cadastroPessoa(txtNome.Text, txtEmail.Text, txtSenha.Text);
+                    llBasico.Visibility = ViewStates.Gone;
+                    llEndereco.Visibility = ViewStates.Visible;
                 }
+            };
 
+			btnConfirmar.Click += (object sender, EventArgs e) =>
+			{
+                p.Cep = Convert.ToInt32(txtCep.Text);
+                p.Estado = txtEstado.Text;
+                p.Cidade = txtCidade.Text;
+                p.Endereco = txtEndereco.Text;
+                p.Numero = Convert.ToInt32(txtNumero.Text);
+                p.Complemento = txtComplemento.Text;
+                p.Telefone = Convert.ToInt32(txtTelefone.Text);
+
+                cadastroPessoa(p);
             };
 
             btnVoltar.Click += (object sender, EventArgs e) =>
             {
+                llBasico.Visibility = ViewStates.Visible;
+                llEndereco.Visibility = ViewStates.Gone;
 
                 //Redireciona para a página de Login
-                var intent = new Intent(this, typeof(MainActivity));
-                StartActivity(intent);
-
+                //var intent = new Intent(this, typeof(MainActivity));
+                //StartActivity(intent);
             };
 
         }
 
-        public void cadastroPessoa(string nome, string email, string senha)
+        public void cadastroPessoa(Pessoa p)
         {
             MySqlConnection con = new MySqlConnection("Server=mysql873.umbler.com;Port=41890;database=ufscarpds;User Id=ufscarpds;Password=ufscar1993;charset=utf8");
 
@@ -69,10 +118,17 @@ namespace ChiquitoMarmoraria
                 {
                     con.Open();
                     Console.WriteLine("Conectado com sucesso!");
-                    MySqlCommand cmd = new MySqlCommand("INSERT INTO pessoa (nome, email, senha) VALUES (@nome, @email, @senha)", con);
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO pessoa (nome, email, senha, cep, estado, cidade, endereco, numero, complemento, telefone) VALUES (@nome, @email, @senha, @cep, @estado, @cidade, @endereco, @numero, @complemento, @telefone)", con);
                     cmd.Parameters.AddWithValue("@nome", txtNome.Text);
                     cmd.Parameters.AddWithValue("@email", txtEmail.Text);
                     cmd.Parameters.AddWithValue("@senha", txtSenha.Text);
+                    cmd.Parameters.AddWithValue("@cep", p.Cep);
+                    cmd.Parameters.AddWithValue("@estado", p.Estado);
+                    cmd.Parameters.AddWithValue("@cidade", p.Cidade);
+                    cmd.Parameters.AddWithValue("@endereco", p.Endereco);
+                    cmd.Parameters.AddWithValue("@numero", p.Numero);
+                    cmd.Parameters.AddWithValue("@complemento", p.Complemento);
+                    cmd.Parameters.AddWithValue("@telefone", p.Telefone);
                     cmd.ExecuteNonQuery();
                     txtNome.Text = "";
                     txtEmail.Text = "";
--- ChiquitoMarmoraria/Resources/model/Pessoa.cs
+++ ChiquitoMarmoraria/Resources/model/Pessoa.cs
@@ -18,6 +18,13 @@ namespace ChiquitoMarmoraria.Resources.model
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
+        public int Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public int Telefone { get; set; }
 
     }
 }
